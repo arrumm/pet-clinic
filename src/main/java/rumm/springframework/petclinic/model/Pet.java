9eac0f96ba4b9d542e2ab987386e0084cdc8832f
Roman Arkhipov <aar.rumm@gmail.com>
2020-05-11 22:57:19 +0200

    adding POJOs, Closes #1

---
+++ src/main/java/rumm/springframework/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package rumm.springframework.petclinic.model;
+
+import java.util.Date;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private Date birthDay;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(final PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(final Owner owner) {
+        this.owner = owner;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(final Date birthDay) {
+        this.birthDay = birthDay;
+    }
+}
---
+++ src/main/java/rumm/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package rumm.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
