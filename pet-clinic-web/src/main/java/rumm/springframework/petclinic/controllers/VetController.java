5f371f88ea92079ce31979ce8f51a29173f4166b
Roman Arkhipov <aar.rumm@gmail.com>
2020-05-17 22:34:41 +0200

    Add VetController Add vets list template page Closes #6

---
+++ pet-clinic-web/src/main/java/rumm/springframework/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package rumm.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String ListVets() {
+        return "vets/index";
+    }
+
+}
