3f53244f4152f5849fec6c8e42583379884cb1db
Roman Arkhipov <aar.rumm@gmail.com>
2020-05-20 09:49:58 +0200

    List all Vets on vets index page Closes #15

--- pet-clinic-web/src/main/java/rumm/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/rumm/springframework/petclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package rumm.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import rumm.springframework.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(final VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", "", "/index", "/index.html"})
-    public String ListVets() {
+    public String ListVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
