51d79ce5c33f5444454e5d0336f2de5cb7b6bdfa
Roman Arkhipov <aar.rumm@gmail.com>
2020-05-22 11:26:57 +0200

    Add Speciality, assign it to Vet Closes #27

---
+++ pet-clinic-data/src/main/java/rumm/springframework/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package rumm.springframework.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+}
