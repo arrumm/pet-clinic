ba11306e3c38fdb9b46add4766e58430697f2f0e
Roman Arkhipov <aar.rumm@gmail.com>
2020-06-03 22:15:35 +0200

    update Visit entity JPA annotations Closes #38

--- pet-clinic-data/src/main/java/rumm/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/rumm/springframework/petclinic/model/Visit.java
@@ -2,10 +2,24 @@ package rumm.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @JoinColumn(name = "pet_id")
+    @ManyToOne
     private Pet pet;
 
     public LocalDate getDate() {
