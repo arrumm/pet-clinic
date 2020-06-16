8d55393f751d9918a8c8ce3913576eb90f607753
Roman Arkhipov <aar.rumm@gmail.com>
2020-06-16 08:23:52 +0200

    Upgrade to JUnit5 add test for testing dependency is working closes #3

---
+++ pet-clinic-web/src/test/java/rumm/springframework/petclinic/PetClinicApplication_when_is_called.java
@@ -0,0 +1,27 @@
+package rumm.springframework.petclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PetClinicApplication_when_is_called {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+}
