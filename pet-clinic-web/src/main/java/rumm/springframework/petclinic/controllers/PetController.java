2746e16a1cb07e6bcec618fb90462fee1672ff3f
Roman Arkhipov <aar.rumm@gmail.com>
2020-08-11 23:05:46 +0200

    add model attributes to pet controller #56

---
+++ pet-clinic-web/src/main/java/rumm/springframework/petclinic/controllers/PetController.java
@@ -0,0 +1,47 @@
+package rumm.springframework.petclinic.controllers;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import rumm.springframework.petclinic.model.Owner;
+import rumm.springframework.petclinic.model.PetType;
+import rumm.springframework.petclinic.services.OwnerService;
+import rumm.springframework.petclinic.services.PetService;
+import rumm.springframework.petclinic.services.PetTypeService;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(final PetService petService, final OwnerService ownerService, final PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+}
